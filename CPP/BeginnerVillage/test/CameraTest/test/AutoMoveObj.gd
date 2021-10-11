64d9ac2e7543b9ccfcbb6c65f3762544f57a0671
chamomy127 <82834366+chamomy127@users.noreply.github.com>
2021-10-11 22:40:24 +0900

    chamge camera manager to merge

--- CPP/BeginnerVillage/test/CameraTest/test/AutoMoveObj.gd
+++ CPP/BeginnerVillage/test/CameraTest/test/AutoMoveObj.gd
@@ -2,27 +2,28 @@ extends Area2D
 
 export var g_distance=200
 export var g_target=0
-var g_direction=1
+onready var g_direction=1
 
+onready var g_mypath = get_path()
 signal traceStart(path,name)
 
-# 임의로 자동으로 움직이는 것 구현. 
 func _ready():
-	g_direction=1
 	g_target=position.x+g_distance*g_direction
-	print(get_node("/root/CameraTest/UIManager").get_child_count())
 	
 func _process(_delta):
-	if(position.x==g_target):
-		g_direction=g_direction*-1
-		g_target = position.x+g_direction*g_distance
+	if position.x == g_target:
+		g_direction = g_direction * -1
+		g_target = position.x + g_direction * g_distance
 	else:
-		position.x+=g_direction
+		position.x += g_direction
 
 # 실제로 사용되는 부분 - 클릭됐을시 자신의 path를 넘겨주는 함수
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.button_index==BUTTON_LEFT and event.is_pressed():
-			print(name)
-			emit_signal("traceStart",get_path(),name)
+		if event.button_index == BUTTON_LEFT and event.is_pressed():
+			print(get_tree().get_current_scene())
+			
+			get_node("/root/CameraTest/UIManager").StartNPCTrace(name)
+			get_node("/root/CameraTest/CameraManager").SetCameraSetting_Trace(g_mypath)
+			#emit_signal("traceStart",get_path(),name)
 		#get_node("/root/UIManager").StartNPCTrace(get_path(),name)
--- CPP/BeginnerVillage/test/CameraTest/test/UIWindowManager.gd
+++ CPP/BeginnerVillage/test/CameraTest/test/UIWindowManager.gd
@@ -1,9 +1,9 @@
-extends CanvasLayer
+extends Node2D
 
 # NPC들을 눌렀을 때 정보 UI창을 띄워주는 매니저가 있고, 각각에 따라가기 아이콘(예:돋보기모양)이 있다고 가정.
 # 상위의 매니저가 UI창들의 리스트를 가지고 있고, 입력 신호를 받았을 때 노드의 정보를 UI창으로 넘긴다고 생각
 
-onready var g_stopfollowing_button = get_node("StopFollowing")
+onready var g_stopfollowing_button = get_node("UILayer/StopFollowing")
 var g_ui_list =[]
 var g_nowchild_index=0
 var g_maxchild_index=1
