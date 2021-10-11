b0925a3a2e9adf92409efb3d4b88225ff3d902ed
chamomy127 <82834366+chamomy127@users.noreply.github.com>
2021-10-11 21:45:14 +0900

    add another restore list

--- CPP/BeginnerVillage/test/CameraTest/camera.gd
+++ CPP/BeginnerVillage/test/CameraTest/camera.gd
@@ -1,24 +1,23 @@
 extends Node2D
 
-export (Vector2)var nowAddVector = Vector2(0,0)
+export (Vector2)var nowPosition = Vector2(0,0)
 export (Vector2)var nowScreenSize = Vector2(0,0)
+
 export var level=0
 export var speed = 10
+
+var velocity = Vector2()
 func _ready():
 	nowScreenSize=get_viewport().get_visible_rect().size
-	level=$CanvasLayer.getSkillLevel()
-	print(level)
 	$Camera2D.SetScreenSize(nowScreenSize)
+	
+func _process(_delta):
+	$Camera2D.position = $EmptyCamera.position
 
-func _input(ev):
-	if ev.is_key_pressed("ui_left"):
-		$Camera2D.CameraMove_Left()
-	if ev.is_key_pressed("ui_right"):
-		$Camera2D.CameraMove_Right()
-	if ev.is_key_pressed("ui_up"):
-		$Camera2D.CameraMove_Up()
-	if ev.is_key_pressed("ui_down"):
-		$Camera2D.CameraMove_Down()
-		
-	nowAddVector = $Camera2D.GetAddVector()
-	position = nowAddVector.normalized() * speed
+func SetCameraTarget(pos):
+	$Camera2D.SetTarget(pos)
+	
+func ReleaseCameraTarget(pos):
+	$Camera2D.ReleaseTarget(pos)
+
+
--- CPP/NativeLib/NativeLib/TestManager.h
+++ CPP/NativeLib/NativeLib/TestManager.h
@@ -1,15 +1,22 @@
 #pragma once
 #include "Common.h"
 #include "testProperty.h"
-#include <CanvasLayer.hpp>
+#include <Node.hpp>
 
-class TestManager : public CanvasLayer
+class TestManager : public Node
 {
-	GODOT_CLASS(TestManager, CanvasLayer);
+	GODOT_CLASS(TestManager, Node);
 
 private:
 	Skill skill = Skill(0, "noname");
 	int test_c;
+
+	float screenBounds_left;
+	float screenBounds_right;
+	float screenBounds_up;
+	float screenBounds_down;
+
+	void DetectScreenBounds(Vector2 newScreenSize);
 
 public:
 	static void _register_methods();
@@ -25,5 +32,10 @@ public:
 	godot::String getSkillName() { return skill.GetName(); }
 	int getTestDataA() { return skill.GetDataA(); }
 	bool getTestDataB() { return skill.GetDataB(); }
+	
+	void SetScreenSize(Vector2 nowScreenSize);
+	void SetTarget(Vector2 pos);
+	void ReleaseTarget(Vector2 pos);
+
 };
 
