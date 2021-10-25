8bbcf7460c6e4d369e4cac66366fd3c6e1217fda
chamomy127 <82834366+chamomy127@users.noreply.github.com>
2021-10-26 02:49:00 +0900

    camera fix
    카메라 매니저 스크립트 c++로 옮기는중

--- CPP/BeginnerVillage/test/CameraTest/CameraManager.gd
+++ CPP/BeginnerVillage/test/CameraTest/CameraManager.gd
@@ -1,14 +1,17 @@
 extends Node2D
 
 enum eCameraState {DEFAULT, TRACE}
+
 export (eCameraState)var g_nowcamerastate
 export var g_velocity=Vector2()
 export var g_speed=3
-export (Vector2)var g_unmoving_size
+
+var g_unmoving_size
 var g_unmoving_interval=100
 
 var targetNode : Node2D
 var g_nowtarget_path
+
 var g_screen_size
 var g_screen_boundary_lefttop
 var g_screen_boundary_rightbottom
@@ -31,17 +34,19 @@ func SetLimit():
 	g_screen_size = get_viewport_rect().size
 	g_unmoving_size = g_screen_size-Vector2(g_unmoving_interval,g_unmoving_interval)
 	
+	#in gd	
 	$Camera2D.limit_left = -g_screen_size.x
 	$Camera2D.limit_right = g_screen_size.x
 	$Camera2D.limit_top = -g_screen_size.y
 	$Camera2D.limit_bottom = g_screen_size.y
 
+#in gd	
 func DetectCameraMoveObj():
 	if g_nowcamerastate == eCameraState.DEFAULT:
 		MoveCamera()
 	else:
 		$Camera2D.position = targetNode.position
-		
+#in gd	
 func MoveCamera():
 	CameraMovewithKey()
 	CameraMovewithMouse()
@@ -51,7 +56,7 @@ func CameraMovewithKey():
 	if g_velocity.length()>0:
 		g_velocity = g_velocity.normalized()*g_speed
 		$Camera2D.position+=g_velocity
-		
+#in gd
 func DetectKeyPress():
 	g_velocity = Vector2(0,0)
 	if Input.is_action_pressed("ui_left"):
@@ -74,6 +79,7 @@ func CameraMovewithMouse():
 		addPos.y=-g_speed
 	elif (nowmousevector.y>g_unmoving_size.y):
 		addPos.y=g_speed
+		
 	$Camera2D.position += addPos	
 	
 # Camera Set ====	
@@ -93,10 +99,11 @@ func SetCameraSetting_Trace(newtracing_path):
 	Zoom(kzoom_in)
 
 # ZOOM ====
+#in gd
 func DetectZoomScrollKey():
 	if g_nowcamerastate == eCameraState.DEFAULT:
 		ZoomScroll()
-		
+#in gd
 func ZoomScroll():
 	if Input.is_action_just_released("wheel_down"):
 		ZoomOut()
@@ -112,7 +119,7 @@ func ZoomIn():
 	var next_scrollfactor = $Camera2D.zoom.x - kzoom_scrollfactor
 	if next_scrollfactor > kzoom_min:
 		Zoom(next_scrollfactor)
-
+#in gd
 func Zoom(zoomfactor):
 	$Camera2D.zoom.x = zoomfactor
 	$Camera2D.zoom.y = zoomfactor
@@ -120,6 +127,4 @@ func Zoom(zoomfactor):
 # test와 연관된 부분, 그만 따라가기 버튼을 누르면 카메라가 줌아웃되는 것
 func _on_StopFollowing_pressed():
 	SetCameraSetting_Default()
-
-	
 
