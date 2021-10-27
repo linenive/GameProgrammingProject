58d40cff8fefa0137d1a3a6a2d1917b8df2fcc16
chamomy127 <82834366+chamomy127@users.noreply.github.com>
2021-10-27 22:38:04 +0900

    Revert "파일 정리"
    This reverts commit 922ddfb1ab75bb743c730dd45b0810058a3183c4.

---
+++ CPP/BeginnerVillage/test/CameraTest/test/CameraTest.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+#func _on_AutoMoveObj_traceStart(path, name):
+	#TraceStart(path, name)
+
+func _on_StopFollowing_pressed():
+	$UIManager.DeactiveStopFollowingButton()
+	$CameraManager.SetCameraSetting_Default()
+	
+#func _on_AutoMoveObj2_traceStart(path, name):
+	#TraceStart(path, name)
+
+func TraceStart(path, name):
+	$UIManager.StartNPCTrace(name)
+	$CameraManager.SetCameraSetting_Trace(path)
