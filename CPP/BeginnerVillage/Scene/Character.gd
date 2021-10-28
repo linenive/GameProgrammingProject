a92c451f6f7114c9e7a6615838af65a0824daf64
chamomy127 <82834366+chamomy127@users.noreply.github.com>
2021-10-28 15:26:03 +0900

    yojin-6/#3 메인의 기능 추가, 플레이어 클릭으로 이동하는 기능  병합

--- CPP/BeginnerVillage/Scene/Character.gd
+++ CPP/BeginnerVillage/Scene/Character.gd
@@ -1,17 +1,22 @@
 extends Sprite
 
 var uicontrol
+var camera_manager
 var character_id : int
+var character_path
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	uicontrol = get_node("/root/Main/UIControl")
-
+	camera_manager = get_node("/root/Main/CameraManager")
+	character_path = get_path()
+	
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
 		if get_rect().has_point(to_local(event.position)):
 			#uicontrol.show_info_popup(get_instance_id(), "Character", get_instance_id(), get_position())
 			uicontrol.show_info_popup(self, "Character")
+			#camera_manager.SetCameraSetting_Trace(character_path)
 
 func init_character(character_id):
 	self.character_id = character_id
