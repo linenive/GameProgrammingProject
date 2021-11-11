35d42eba5cea1d7197ab060858b1a94075c16ba9
linenive <linenive@naver.com>
2021-11-11 20:25:30 +0900

    Update JanTestGDScript.gd

--- CPP/BeginnerVillage/Scene/JanTestGDScript.gd
+++ CPP/BeginnerVillage/Scene/JanTestGDScript.gd
@@ -6,6 +6,10 @@ func _ready():
 	ui_manager = get_node("/root/Main/UIManager")
 
 func _process(delta):
+	#GetEventLogExample()
+	pass
+	
+func GetEventLogExample():
 	var logs = ui_manager.GetEventLogs()
 	for single_log in logs:
 		print(single_log[0] + ": "+single_log[1])
@@ -13,6 +17,3 @@ func _process(delta):
 		print(String(single_log[2])+"년 "+String(single_log[3])+"월 "+\
 			String(single_log[4])+"일 "+ String(single_log[5])+":"+\
 			String(single_log[6]))
-	#print(logs[0])
-	#print(logs[1])
-	#print(logs[2])
