2ad5ff0800270d83ad4ef4d627457af7eb95706e
linenive <linenive@naver.com>
2021-11-13 18:04:45 +0900

    Skill System

--- CPP/NativeLib/NativeLib/Skill.h
+++ CPP/NativeLib/NativeLib/Skill.h
@@ -6,10 +6,14 @@ using namespace std;
 
 class Skill {
 private:
-    string skill_name;
+	string skill_name;
 public:
-    Skill(string skill_name, Stat stat) : skill_name(skill_name), stat(stat) {}
-    string GetSkillName() {return skill_name;}
+	Skill(string skill_name, Stat stat) : skill_name(skill_name), stat(stat) {}
+	string GetSkillName() { return skill_name; }
+	Skill& Add(eStatFieldName field_name, int field_value) {
+		stat.AddStatFieldValue(field_name, field_value);
+		return *this;
+	}
 
-    Stat stat;
+	Stat stat;
 };
