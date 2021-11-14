6e4963dad7d654d2d4cb3819ec674e7c3d174cc3
linenive <linenive@naver.com>
2021-11-14 23:42:17 +0900

    Update Character.h

--- CPP/NativeLib/NativeLib/Character.h
+++ CPP/NativeLib/NativeLib/Character.h
@@ -3,12 +3,33 @@
 #include "Inventory.h"
 #include "Material.h"
 #include "Schedule.h"
+#include "Skill.h"
+#include "Stat.h"
+#include <vector>
+
+enum eLevel {
+	LEVEL1, LEVEL2, LEVEL3
+};
+
+enum eGender {
+	MAN, WOMAN
+};
 
 class Character : public WorldObject {
 
 private:
 	Schedule* schedule;
 	Inventory* inventory;
+    eLevel level;
+    eGender gender;
+    vector<Skill> skill_list;
+    Task* current_task;
+    Stat base_stat;
+
+    void SetRandomSkill();
+    void AddSkill(Skill new_skill);
+    void SwitchSkill(int old_skill_index, Skill new_skill);
+    void SetBaseStat();
 
 public:
 	~Character() {
