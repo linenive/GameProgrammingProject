ccd672f5f345bab96bc496775093660b79d3305b
linenive <linenive@naver.com>
2021-11-16 21:30:42 +0900

    string 관련 에러 제거

--- CPP/NativeLib/NativeLib/CharacterFactory.h
+++ CPP/NativeLib/NativeLib/CharacterFactory.h
@@ -1,32 +1,20 @@
 #pragma once
-#include "Base.h"
 #include "WorldObjectFactory.h"
 #include "Character.h"
 
+// Todo: hard coding -> load DB
 class PurposeOfVisitFactory {
-private:
-	static string name[10];
-	static string NameOf(ePurposeOfVisitType type) {
-		return name[static_cast<int>(type)];
-	};
-	//To-do: init by external file
-	static void Init() {
-		name[static_cast<int>(ePurposeOfVisitType::Lodge)] = "Lodge";
-		name[static_cast<int>(ePurposeOfVisitType::JustChilling)] = "JustChilling";
-		name[static_cast<int>(ePurposeOfVisitType::Recovery)] = "Recovery";
-		name[static_cast<int>(ePurposeOfVisitType::Cure)] = "Cure";
-		name[static_cast<int>(ePurposeOfVisitType::Shopping)] = "Shopping";
-		name[static_cast<int>(ePurposeOfVisitType::BuyLiquidMedicine)] = "BuyLiquidMedicine";
-	}
 public:
-	PurposeOfVisitFactory() {
-		Init();
-	}
 	PurposeOfVisit* CreatePurposeOfVisit(ePurposeOfVisitType type) {
 		PurposeOfVisit* purpose;
-		purpose = new PurposeOfVisit(NameOf(type), type);
+		if (type == ePurposeOfVisitType::Lodge) {
+			purpose = new PurposeOfVisit("Lodge", type);
+		}
+		else {
+			purpose = new PurposeOfVisit("NoName", type);
+		}
 		return purpose;
-	}	
+	}
 };
 
 class CharacterFactory : public WorldObjectFactory {
@@ -34,12 +22,12 @@ private:
 	PurposeOfVisitFactory purpose_factory;
 	// To-do: game progress에 따라 guest들이 가지는 number of purpose가 늘어나고,
 	// purpose type들도 Unlock될 수 있도록 만든다.
-	
+
 	void SettingPurposeOfVisit(Character* character) {
 		// To-do: number of purpose를 random으로 정하도록 바꾼다.
 		int number_of_purpose = 2;
 		for (int i = 0; i < number_of_purpose; i++) {
-			GuestSchedule* schedule = (GuestSchedule *)character->GetSchedule();
+			GuestSchedule* schedule = (GuestSchedule*)character->GetSchedule();
 			// To-do: Probability가 포함된 pool에서 random type을 pick하도록 바꾼다.
 			// To-do: 한 번 picked 된 type은 delete in pool.
 			schedule->AddPurposOfVisit(purpose_factory.CreatePurposeOfVisit(ePurposeOfVisitType::Lodge));
