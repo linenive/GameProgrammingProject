1a13de05daa27491658713f510556fcbbdd811ce
linenive <linenive@naver.com>
2021-11-24 16:37:38 +0900

    StaticUnitService 에서 GameWorldFor~~ 제거

--- CPP/NativeLib/NativeLib/StaticUnitService.h
+++ CPP/NativeLib/NativeLib/StaticUnitService.h
@@ -3,14 +3,16 @@
 #include "Common.h"
 #include "Base.h"
 #include "BuildingType.h"
-#include "Building.h"
 #include "CoordinatesSystem.h"
+#include "TileRepository.h"
+#include "BuildingRepository.h"
 #include <Node.hpp>
 
 class StaticUnitService {
 
 private:
-	GameWorldForStaticUnit* game_world;
+	TileRepository* tile_repository;
+	BuildingRepository* building_repository;
 	int next_building_id;
 	int next_structure_id;
 
@@ -18,6 +20,11 @@ private:
 	void RegisterBlocksToWorld(int start_x, int start_y, vector< vector<eBlockType> >& blocks, Building* building);
 	bool IsPlacablePosition_(int start_x, int start_y, vector< vector<eBlockType> >& blocks);
 	void HideBuildingBlocks(Building* building);
+
+	Tile* GetTile(int pos_x, int pos_y) { return tile_repository->GetTile(pos_x, pos_y); };
+	Building* GetBuildingById(int id) { return building_repository->GetBuildingById(id); }
+	void AddBuilding(Building* building) { building_repository->AddBuilding(building); }
+	void DeleteBuildingFromWorld(int id) { building_repository->DeleteBuildingById(id); }
 
 public:
 	int CreateBuilding(int type, Vector2 top_left_tile_position);
@@ -27,6 +34,6 @@ public:
 	void DeleteBuildingById(int id);
 	vector<Coordinates> GetBuildingBlocksCoordinatesById(int id);
 
-	StaticUnitService(GameWorldForStaticUnit* world);
-	StaticUnitService(GameWorldForStaticUnit* world, int start_building_id, int start_structur_id);
+	StaticUnitService(TileRepository* tile_repo, BuildingRepository* building_repo);
+	StaticUnitService(TileRepository* tile_repo, BuildingRepository* building_repo, int start_building_id, int start_structur_id);
 };
