d80f3b4cd540efbd16fcac39f85fa0575e8ebc60
linenive <linenive@naver.com>
2021-11-27 01:43:56 +0900

    jan-23 ResidentService 추가

---
+++ CPP/NativeLib/NativeLib/ResidentService.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include "ObjectRepository.h"
+#include "BuildingRepository.h"
+#include "House.h"
+#include "WorkSpace.h"
+
+class ResidentService {
+private:
+	ObjectRepository* object_repo;
+	BuildingRepository* building_repo;
+
+	void AssignCharacterToBuilding(Character* villager, int building_id) {
+		Building* building = building_repo->GetBuildingById(building_id);
+		building->AssignCharacter(villager);
+	}
+
+public:
+	ResidentService(ObjectRepository* object_repo, BuildingRepository* building_repo) 
+		: object_repo(object_repo), building_repo(building_repo) {}
+	
+	void AssignResidentToHome(int resident_id, int home_id) {
+		Character* resident = object_repo->GetCharacter(resident_id);
+		resident->home_id = home_id;
+
+		AssignCharacterToBuilding(resident, home_id);
+	}
+
+	void AssignResidentToWorkSpace(int resident_id, int work_space_id) {
+		Character* resident = object_repo->GetCharacter(resident_id);
+		resident->work_space_id = work_space_id;
+
+		AssignCharacterToBuilding(resident, work_space_id);
+	}
+
+
+	void FreeFromHome(int  resident_id) {
+		Character* resident = object_repo->GetCharacter(resident_id);
+		Building* home = building_repo->GetBuildingById(resident->home_id);
+
+		home->FreeResident(resident_id);
+		resident->home_id = -1;
+	}
+
+	void FreeFromWorkSpace(int resident_id) {
+		Character* resident = object_repo->GetCharacter(resident_id);
+		Building* work_space = building_repo->GetBuildingById(resident->home_id);
+
+		work_space->FreeResident(resident_id);
+		resident->work_space_id = -1;
+	}
+
+	Vector2 GetResidentHomePosition(int resident_id) {
+		Character* resident = object_repo->GetCharacter(resident_id);
+		if (resident->home_id == -1) {
+			printf("[ResidentService]ERROR: this resident doesn't have home! id: %d\n", resident_id);
+			return Vector2(10, 10); //dummy position
+		}
+		else {
+			Building* home = building_repo->GetBuildingById(resident->home_id);
+			return home->GetCenterPosition();
+		}
+	}
+
+	Vector2 GetResidentWorkPlacePosition(int resident_id) {
+		Character* resident = object_repo->GetCharacter(resident_id);
+		if (resident->work_space_id == -1) {
+			printf("[ResidentService]ERROR: this resident doesn't have work place! id: %d\n", resident_id);
+			return Vector2(10, 10); //dummy position
+		}
+		else {
+			Building* work_place = building_repo->GetBuildingById(resident->work_space_id);
+			return work_place->GetCenterPosition();
+		}
+	}
+};
