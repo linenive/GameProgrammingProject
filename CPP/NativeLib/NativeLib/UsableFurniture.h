6b255b383f51f289cad09c7f68ffaaa542d773f6
chamomy127 <82834366+chamomy127@users.noreply.github.com>
2021-11-28 15:24:07 +0900

    사용하는 가구, 전시용 가구, 장착 아이템 추가

---
+++ CPP/NativeLib/NativeLib/UsableFurniture.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Furniture.h"
+#include "Character.h"
+
+class UsableFurnitureItem : public FurnitureItem {
+private:
+	Character* current_use_character = nullptr;
+
+public:
+	~UsableFurnitureItem() {
+		delete current_use_character;
+	}
+	bool IsVacant() {
+		return current_use_character == nullptr;
+	}
+	bool SetOccupiedCharacter(Character* new_character) {
+		if (IsVacant()){
+			current_use_character = new_character;
+			return true;
+		}
+		return false;
+	}
+	Character* LeaveOccupiedCharacter() {
+		if (IsVacant()) return nullptr;
+
+		Character* last_character = current_use_character;
+		current_use_character = nullptr;
+
+		return last_character;
+	}
+};
