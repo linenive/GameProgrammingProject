09ca0e8346d4bf1c70dd40b8033bcb7255f7fa1a
chamomy127 <82834366+chamomy127@users.noreply.github.com>
2021-12-03 18:07:25 +0900

    item에서 기능부분삭제, stat parsing 추가

--- CPP/NativeLib/NativeLib/IDService.h
+++ CPP/NativeLib/NativeLib/IDService.h
@@ -18,9 +18,11 @@ public:
 	IDService(){
 		type_list["Material"] = 0;
 		type_list["Furniture"] = 1;
-		type_list["Bed"] = 2;
-		type_list["DisplayStand"] = 3;
-		type_list["Weapon"] = 4;
+		type_list["Bed"] = 1;
+		type_list["DisplayStand"] = 1;
+		type_list["Weapon"] = 2;
+		type_list["Armor"] = 2;
+		type_list["Potion"] = 2;
 		
 		for (auto elem : type_list) {
 			id_list.insert(make_pair(elem.second,IDRepository(elem.second)));
