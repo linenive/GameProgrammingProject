c89945c0a09b3dbf0eecfd25bc75a0eca7df04e8
chamomy127 <82834366+chamomy127@users.noreply.github.com>
2021-12-04 20:58:58 +0900

    itemdictionary 소멸자 private로함

--- CPP/NativeLib/NativeLib/ItemDictionary.h
+++ CPP/NativeLib/NativeLib/ItemDictionary.h
@@ -11,6 +11,9 @@ private:
 	unordered_map<int, Item*> id_item_list;
 	unordered_map<string, Item*> name_item_list;
 	static ItemDictionary* instance;
+	~ItemDictionary() {
+		delete instance;
+	}
 
 public:
 	static ItemDictionary* GetInstance();
@@ -18,9 +21,7 @@ public:
 	ItemDictionary();
 	ItemDictionary(const ItemDictionary& ref){}
 	ItemDictionary& operator=(const ItemDictionary& ref){}
-	~ItemDictionary() {
-		delete instance;
-	}
+
 	void GenerateDictionary();
 	Item* MakeItem(string type, vector<string> sub_line_list);
 	unordered_map<string, int> ParameterParsing(vector<string> sub_line_list);
