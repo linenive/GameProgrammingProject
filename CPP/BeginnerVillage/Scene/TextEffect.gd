858ee9cb3a3921e36adbdc877b85267ecd122b57
chamomy127 <82834366+chamomy127@users.noreply.github.com>
2021-12-10 09:53:38 +0900

    움직이는것을 delta곱으로 바꿈

--- CPP/BeginnerVillage/Scene/TextEffect.gd
+++ CPP/BeginnerVillage/Scene/TextEffect.gd
@@ -36,7 +36,7 @@ func _process(_delta):
 	if left_time <= 0:
 		queue_free()
 	elif left_time<=0.5:
-		$Text.rect_position += Vector2(0.1, -0.1)
+		$Text.rect_position += Vector2(1, -1)*_delta * 20
 		left_time -= _delta
 		current_fade -= _delta * text_length * 3
 		FadeTextUpdate()
